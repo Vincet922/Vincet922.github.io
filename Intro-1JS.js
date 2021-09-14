e0fbbb129cf172482081a517aeaae77716e31b1c
Vincet922 <65345320+Vincet922@users.noreply.github.com>
2021-09-13 21:51:23 -1000

    Add files via upload

---
+++ Intro-1JS.js
@@ -0,0 +1,17 @@
+function hover(){
+	var title1 = document.getElementById("title-1");
+	title1.style.opacity="0.7";
+	title1.style.cursor="pointer";
+}
+
+
+function out(){
+	var title1 = document.getElementById("title-1");
+	title1.style.opacity="1";
+	title1.style.color="";
+	title1.style.cursor="";
+}
+
+function Click(){
+	window.location = "Mainpage.html";
+}
